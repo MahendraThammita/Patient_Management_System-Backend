5b4a38e9d3613af1672f1bca46a2a9592f36d0d9
UdithaJ <udithaj.98@gmail.com>
2021-08-16 14:19:16 +0530

    Scheduling updated

--- routes/DoctorRoute.js
+++ routes/DoctorRoute.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const Doctor = require('../modals/Doctor');
+const Schedule = require('../modals/Schedule')
 const bcrypt = require("bcrypt");
 
 router.post("/add", async (req,res) => {
@@ -36,6 +37,38 @@ router.post("/add", async (req,res) => {
         }).catch((err) => {
             return res.status(400).json(err);
         })
+    }
+
+})
+
+router.put("/add-time-slot/:doctorID", async (req,res) => {
+
+    const doctorID = req.params.doctorID;
+
+    let timeSlot = req.body.timeSlot;
+    let status = req.body.status;
+
+    const isExisting = await Doctor.findById(doctorID).find({timeSlots: {$elemMatch: {timeSlot:timeSlot}}});
+    console.log(isExisting);
+
+    if (isExisting.length !== 0){
+        return res.status(401).json("Duplicated time slot!");
+    }
+    else{
+
+        const schedule = new Schedule({
+
+            timeSlot: timeSlot,
+            status: status,
+
+        });
+
+        await Doctor.findOneAndUpdate({'_id':doctorID}, {"$push":{"timeSlots":schedule}}).then(() => {
+            return res.status(201).json("Time Slot Added ");
+        }).catch((err) => {
+            console.log(err);
+        })
+
     }
 
 })
