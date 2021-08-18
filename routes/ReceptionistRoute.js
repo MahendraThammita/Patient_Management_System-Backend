3b8b3efeab585a6cc899cead6c277ea81a4993e9
UdithaJ <udithaj.98@gmail.com>
2021-08-18 23:29:32 +0530

    recep profile update

--- routes/ReceptionistRoute.js
+++ routes/ReceptionistRoute.js
@@ -60,4 +60,44 @@ router.post("/login", async(req, res) => {
     }
 })
 
+router.put("/update/:userID", async (req,res) => {
+
+    try{
+
+        let userID = req.params.userID;
+        let username = req.body.username;
+        let mobileNumber = req.body.mobileNumber;
+        let oldPassword = req.body.oldPassword;
+        let newPassword = req.body.newPassword;
+
+        const user = await Receptionist.findOne({_id:userID});
+        const isTrue = await bcrypt.compare(oldPassword, user.password);
+
+        if (isTrue) {
+            const salt = await bcrypt.genSalt();
+            const hash = await bcrypt.hash(newPassword, salt);
+            const updatedValue = {
+
+                username: username,
+                mobileNumber: mobileNumber,
+                password: hash
+            };
+
+            const  updateValue = await  Receptionist.findByIdAndUpdate(userID,updatedValue).then(() => {
+                res.json({status:200, message:'successfully updated'})
+            }).catch((err) => {
+                res.json({status:400, error:err})
+            })
+
+    }
+        else {
+            res.json({status:401, message:'Incorrect Password'})
+        }
+    }
+    catch(err){
+        console.log(err);
+    }
+
+})
+
 module.exports = router;
