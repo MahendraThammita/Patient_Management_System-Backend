3c323160559a5c9b93000d0d20793081eb88ae7e
M.L.Akila Liyanage <IT19120812@my.sliit.lk>
2021-08-19 19:00:36 +0530

    fixed #133

--- app.js
+++ app.js
@@ -5,22 +5,28 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const router = express.Router({});
 require('dotenv/config')
+const session = require('express-session')
+var memoryStore = new session.MemoryStore();
+app.use(session({ secret: '63fd9f4d-a693-40cb-acf1-2ff729047d58', resave: false, saveUninitialized: true, store: memoryStore }))
+const keycloak = require('./config/keycloak-config.js').initKeycloak(memoryStore);
 
 const PORT = process.env.PORT || 8090;
 
 //Import Routes
 const Health = require('./routes/HelathCheck')
-// const TestR = require('./routes/test-controller')
+const TestR = require('./routes/test-controller')
+const Logins = require('./routes/LoginRoutes')
 
 //Middleware
-// const keycloak = require('./config/keycloak-config.js').initKeycloak();
-// app.use(keycloak.middleware());
-app.use(bodyParser.json())
 app.use(cors())
+app.use(keycloak.middleware());
+app.use(bodyParser.json())
+
 
 //routes
 app.use('/',Health)
-// app.use('/test',TestR)
+app.use('/test',TestR)
+app.use('/auth',Logins)
 
 //connecting to the database
 mongoose.connect(
