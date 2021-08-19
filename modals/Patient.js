a7ad38f726c354e05db8e5b5dda9447d6d64cfc4
Nethsara Liyanage <tharusharb1012@gmail.com>
2021-08-19 22:22:17 +0530

    fix AB#112

---
+++ modals/Patient.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const  Schema = mongoose.Schema;
+
+const patientSchema = new Schema({
+
+    fullName: {
+        type: String,
+        required: true
+    },
+    suffix: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+    Proffesion: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    guardianName: {
+        type: String,
+        required: false
+    },
+    guardianPhone: {
+        type: String,
+        required: false
+    },
+    guardianEmail: {
+        type: String,
+        required: true
+    }
+})
+const Doctor = mongoose.model("Patient", patientSchema);
+module.exports = Patient;
