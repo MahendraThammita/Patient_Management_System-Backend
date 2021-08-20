69996d6ab4ccedd2bc6c25b14fecf8754a37b98e
Nethsara Liyanage <tharusharb1012@gmail.com>
2021-08-21 00:01:46 +0530

    fix AB#114

---
+++ modals/Appointmnet.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const  Schema = mongoose.Schema;
+
+const appointmentSchema = new Schema({
+
+    fullName: {
+        type: String,
+        required: true
+    },
+    approvedStatus: {
+        type: Boolean,
+        required: true
+    },
+    status:{
+        type:String,
+        required: true
+    },
+    prescription:{
+        type:Array,
+        required: false
+    },
+    patientMessage:{
+        type:String,
+        required:false
+    },
+    reports:{
+        type:Array,
+        required:false
+    },
+    appointmentDate:{
+        type:String,
+        required:true
+    },
+    appointmentTimeSlot:{
+        type:String,
+        required:true
+    },
+    patient:{
+        type: mongoose.Schema.Types.ObjectId, 
+        required: true, 
+        ref: 'patient'
+    },
+    doctor:{
+        type: mongoose.Schema.Types.ObjectId, 
+        required: true, 
+        ref: 'Doctor'
+    },
+    
+    
+})
+
+const Appointment = mongoose.model("appointment", appointmentSchema);
+module.exports = Appointment;
---
+++ routes/AppointmentsRoutes.js
@@ -0,0 +1,5 @@
+const router = require('express').Router();
+const Appointment = require('../modals/Appointmnet');
+
+
+module.exports = router;
