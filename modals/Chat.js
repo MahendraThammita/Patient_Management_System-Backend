3eb3687f2266e68ef9e691831a968d557bba8524
M.L.Akila Liyanage <IT19120812@my.sliit.lk>
2021-09-17 11:41:54 +0530

    chat-doc

---
+++ modals/Chat.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const  Schema = mongoose.Schema;
+
+const chatSchema = new Schema({
+
+    roomId: {
+        type: String,
+        required: true
+    },
+    user1: {
+        type: String,
+        required: true
+    },
+    user2: {
+        type: String,
+        required: true
+    },
+    message: {
+        type: Array,
+        required: false
+    },
+})
+const Chat = mongoose.model("Chat", chatSchema);
+module.exports = Chat;
