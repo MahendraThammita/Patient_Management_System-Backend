cf21c5b6e22b1e5abacd8c0be8b26d58dc5b20d7
unknown <dhmmpthammita@gmail.com>
2021-10-01 15:51:19 +0530

    PDG generating and uploading backend function implementation AB#219

--- Asset/LabReportGenerator.js
+++ Asset/LabReportGenerator.js
@@ -1,5 +1,6 @@
 const PDFGenerator = require('pdfkit')
 const fs = require('fs')
+const moment= require('moment') 
 
 class LabReportGenerator{
     constructor(test) {
@@ -14,10 +15,10 @@ class LabReportGenerator{
             .image('./pmslogodart.png', 50, 50, { width: 250})
             .fillColor('#000')
             .fontSize(20)
-            .text('INVOICE', 275, 50, {align: 'right'})
+            .text('LAB REPORT', 275, 50, {align: 'right'})
             .fontSize(10)
             .text(`Test Report Number: ${Math.random().toString(36).slice(2)}`, {align: 'right'})
-            .text(`Due: ${this.test.date}`, {align: 'right'})
+            .text(`Due: ${moment(this.test.date, "dd-MM-yyyy")}`, {align: 'right'})
             .moveDown()
             .text(`Patient Address:\n ${patient.fullName}\n${patient.addressLine1}\n${patient.addressLine2}\n${patient.city},${patient.email}`, {align: 'right'})
     
@@ -40,14 +41,24 @@ class LabReportGenerator{
         const patientData = this.test.patient;
       
         doc
-          .text(`Patient Name: ${patientData.fullName}`, 50, 200)
-          .text(`Patient Email: ${patientData.email}` , 50, 215)
-          .text(`Patient Age:  ${patientData.age}`, 50, 130)
+          .text(`Patient Name: ${patientData.fullName}`, 50, 210)
+          .text(`Patient Email: ${patientData.email}` , 50, 230)
+          .text(`Patient Age:  ${patientData.age}`, 50, 250)
       
-          .text(`Test Date: ${new Date()}` , 300, 200)
-          .text(`Speciman No: SPEC0159`, 300, 215)
-          .text(`Test Id:  ${patientData._id}`, 300, 130)
+          .text(`Test Date: ${moment(new Date()).format("DD/MM/YYYY")}` , 300, 210)
+          .text(`Speciman No: SPEC0159`, 300, 230)
+          .text(`Test Id:  ${patientData._id}`, 300, 250)
           .moveDown();
+
+          doc.moveTo(50,270)
+            .lineTo(550,270)
+            .stroke()
+            .moveDown()
+
+            .fontSize(15)
+            .text(col1, 70, y)
+            .text(col2, 180, y)
+            .text(col3, 350, y);
       }
 
       generateTableRow(doc, y, col1, col2, col3) {
@@ -55,17 +66,17 @@ class LabReportGenerator{
           .fontSize(10)
           .text(col1, 70, y)
           .text(col2, 180, y)
-          .text(col3, 350, y,);
+          .text(col3, 350, y);
       }
 
       generateResultsTable(doc) {
         let i,
-          resultsTableTop = 330;
+          resultsTableTop = 280;
       
         for (i = 0; i < this.test.results.length; i++) {
           const item = this.test.results[i];
           const position = resultsTableTop + (i + 1) * 30;
-          generateTableRow(
+          this.generateTableRow(
             doc,
             position,
             item.Item,
